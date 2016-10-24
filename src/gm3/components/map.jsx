3c79fb2008168e6df7c68d7141b997719762c2bd
Dan 'Ducky' Little <danlittle@yahoo.com>
2016-10-24 10:31:54 -0500

    Default ratio set to 1.0 for WMS layers to preserve the same behaviour as previous versions of GM

--- src/gm3/components/map.jsx
+++ src/gm3/components/map.jsx
@@ -68,6 +68,7 @@ class Map extends Component {
 
         return {
             url: mapSource.urls[0],
+            ratio: 1.0, // This is a carry over from previous generations behaviour.
             extent: [-13884991, 2870341, -7455066, 6338219],
             params: Object.assign({'LAYERS' : layers.join(',')}, mapSource.params),
             serverType: mapSource.serverType
