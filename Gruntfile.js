acfe8a2d54008e2c679172fe8e1ae595f32e4d36
CaitW <wolters.caitlin@gmail.com>
2017-04-23 13:58:30 -0400

    Fix grunt-contrib-watch
    Only one Grunt watch task can be run at once, therefore when the user
    runs the “grunt serve” task, only watch:gm3 runs. If the watch group is
    run instead, all watch tasks are run.

--- Gruntfile.js
+++ Gruntfile.js
@@ -153,7 +153,7 @@ module.exports = function(grunt) {
 
     grunt.task.registerTask('lint', ['eslint']);
 
-    grunt.task.registerTask('serve', ['webpack-dev-server:start', 'watch:gm3', 'watch:services']);
+    grunt.task.registerTask('serve', ['webpack-dev-server:start', 'watch']);
 
     // only build the non-minified version.
     grunt.task.registerTask('build-dev', ['eslint', 'copy:services', 'webpack:build-dev']);
