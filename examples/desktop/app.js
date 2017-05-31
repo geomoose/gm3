889e6c783a0c305cc9b97a23d8ae418662f7b5ff
Jim Klassen <klassen.js@gmail.com>
2017-05-31 15:31:01 -0500

    Add Dakota County ArcGIS FeatureServer Layers.
    This exposes some bugs:
     1) It appears only the first layer defined is used, the layer name is ignored.
     2) The zoom-to seems broken (maybe becuase data is returned in 3857)?

--- examples/desktop/app.js
+++ examples/desktop/app.js
@@ -67,6 +67,7 @@ app.loadMapbook({url: 'mapbook.xml'}).then(function() {
     app.registerService('select', SelectService, {
         queryLayers: [
             {value: 'ags-vector/0', label: 'Parcel Points (AGS)'},
+            {value: 'ags-vector/20', label: 'Dakota Streets'},
             {value: 'vector-parcels/ms:parcels', label: 'Parcels'}
         ]
     });
