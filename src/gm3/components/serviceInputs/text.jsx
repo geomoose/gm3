0ae186f9216850bd5c213c5d131b552a653941bd
Dan 'Ducky' Little <theduckylittle@gmail.com>
2017-06-07 12:38:38 -0500

    Fix for "forgetting" select setting.
    Old bug: select, change to rail roads, go = rail roads.
             select, shows parcels selected, go = rail roads (should be parcels).
    
    This was because the local "state" in the service manager has the value
    set to rail roads yet parcels are what needed to be set.  This ensures the
    inputs show what the service manager thinks they should be.
    
    Follow up work: put more service info into the state tree.

--- src/gm3/components/serviceInputs/text.jsx
+++ src/gm3/components/serviceInputs/text.jsx
@@ -32,7 +32,7 @@ export default class TextInput extends Component {
         super(props);
 
         this.state = {
-            value: props.field.default ? props.field.default : ''
+            value: props.value ? props.value : props.field.default ? props.field.default : ''
         };
 
         this.onChange = this.onChange.bind(this);
