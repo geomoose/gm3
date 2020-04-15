a566823c395cf2eeb423094b11ccd7d7cbe24296
Dan 'Ducky' Little <theduckylittle@gmail.com>
2020-04-14 21:33:39 -0500

    Add tests for the edit reducer.

---
+++ tests/gm3/reducers/editor.test.js
@@ -0,0 +1,56 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016-2017 Dan "Ducky" Little
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+import { createStore, combineReducers } from 'redux';
+
+import reducer from 'gm3/reducers/editor'
+import {setEditFeature, finishEditing} from 'gm3/actions/edit'
+
+describe('test the `edit` reducer', () => {
+    it('sets and clears the editing feature', () => {
+        const fakeFeature = {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [0, 0],
+            },
+            properties: {},
+        };
+        const fakeSource = 'fake-source';
+
+        let nextState = reducer();
+
+        nextState = reducer(nextState, setEditFeature(fakeSource, fakeFeature));
+        expect(nextState).toEqual({
+            source: fakeSource,
+            feature: fakeFeature,
+        });
+
+        nextState = reducer(nextState, finishEditing());
+        expect(nextState).toEqual({
+            source: '',
+            feature: null,
+        });
+    });
+});
