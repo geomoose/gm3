aa55fa21b7ef8a20bf64abea0fe0e6119613fa32
Dan 'Ducky' Little <theduckylittle@gmail.com>
2020-05-28 22:25:44 -0500

    Add clear results tooltip

--- src/gm3/components/serviceManager.js
+++ src/gm3/components/serviceManager.js
@@ -165,7 +165,13 @@ class ServiceManager extends React.Component {
                 <div className='results-header'>
                     { this.props.t(service_title) }
                     <div className='results-tools'>
-                        <i className='icon clear' onClick={() => { this.props.removeQuery(queryId); }}></i>
+                        <i
+                            title={this.props.t('results-clear')}
+                            className='icon clear'
+                            onClick={() => {
+                                this.props.removeQuery(queryId);
+                            }}>
+                        </i>
                     </div>
                 </div>
                 <div className='results-query-id'>{ queryId }</div>
