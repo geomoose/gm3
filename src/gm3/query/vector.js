1a74a2ded3c8d8aea04a895e17c27f5fcfe01ea4
Duck Little <theduckylittle@gmail.com>
2026-03-24 19:41:37 -0500

    Fix search by adding filters (like, ilike, etc.)

--- src/gm3/query/vector.js
+++ src/gm3/query/vector.js
@@ -24,19 +24,112 @@
 
 import intersects from "@turf/boolean-intersects";
 
-// Build a filter function based on the fields filter
-export const buildFilterFunction = () => {};
+// converts the SQL-style wild cards of '%' and '_' to their equivalent
+//  reg-exp expressions and compiles them
+const likeSQLtoRegExp = (pattern, ignoreCase = false) => {
+  const parts = [];
 
-export const vectorFeatureQuery = (layer, mapState, mapSource, query) => {
-  const selection = query.selection[0];
-  console.log("MS features=", mapSource.features);
-  const queryFeatures = mapSource.features || [];
-  return new Promise((resolve) => {
-    resolve({
-      layer,
-      features: queryFeatures.filter((feature) => {
-        return intersects(selection, feature.geometry);
-      }),
+  const addLiteral = () => {
+    parts.push(RegExp.escape(currentLiteral));
+    currentLiteral = "";
+  };
+
+  let currentLiteral = "";
+  for (let i = 0, len = pattern.length; i < len; i++) {
+    // substring is rollover safe
+    const chr = pattern.substring(i, i + 1);
+    const next = pattern.substring(i + 1, i + 2);
+
+    if (chr === "\\") {
+      // SQL does not have fancy escaping techniques
+      currentLiteral += next;
+      i += 1;
+    } else if (chr === "%") {
+      // SQL wild card, yahoo.
+      addLiteral(currentLiteral);
+      parts.push(".*");
+    } else if (chr === "_") {
+      // this is the single character match
+      addLiteral(currentLiteral);
+      parts.push(".");
+    } else {
+      currentLiteral += chr;
+    }
+  }
+  // ensure any trailing literal bits are added.
+  if (currentLiteral.length > 0) {
+    addLiteral(currentLiteral);
+  }
+
+  const flags = ignoreCase ? "i" : "";
+  const rePattern = parts.join("");
+  return new RegExp(rePattern, flags);
+};
+
+/* This mapping is based on what is available in the WFS module
+ */
+export const FILTER_FUNCTIONS = {
+  like: (filter, ignoreCase = false) => {
+    const re = likeSQLtoRegExp(filter.value, ignoreCase);
+    return (f) => f.properties[filter.name]?.match(re) !== null;
+  },
+  ilike: (filter) => {
+    return FILTER_FUNCTIONS.like(filter, true);
+  },
+  eq: (filter) => {
+    // this is intentionally untype checked!
+    return (f) => f.properties[filter.name] == filter.value;
+  },
+  ge: (filter) => {
+    return (f) => filter.value >= f.properties[filter.name];
+  },
+  gt: (filter) => {
+    return (f) => filter.value > f.properties[filter.name];
+  },
+  le: (filter) => {
+    return (f) => filter.value <= f.properties[filter.name];
+  },
+  lt: (filter) => {
+    return (f) => filter.value < f.properties[filter.name];
+  },
+};
+
+export const vectorFeatureQuery = async (layer, mapState, mapSource, query) => {
+  const filters = [];
+
+  // if a selection is available, use it as a geometry filter
+  const selection = query.selection?.[0];
+  if (selection) {
+    filters.push((feature) => {
+      return intersects(selection, feature.geometry);
     });
+  }
+
+  console.log(query.fields);
+
+  query.fields?.forEach((field) => {
+    if (field.comparitor in FILTER_FUNCTIONS) {
+      filters.push(FILTER_FUNCTIONS[field.comparitor](field));
+    }
   });
+
+  console.log("filters=", filters);
+
+  // return an empty set if no filters are set.
+  if (filters.length < 1) {
+    return {
+      layer,
+      features: [],
+    };
+  }
+
+  // iterate through the features, applying each to the list
+  let features = mapSource.features || [];
+  filters.forEach((filterFn) => {
+    features = features.filter(filterFn);
+  });
+  return {
+    layer,
+    features,
+  };
 };
