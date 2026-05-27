aa036c5c6d8a8e26ced765f6335d4071a69278e4
Duck Little <theduckylittle@gmail.com>
2026-05-26 23:16:11 -0500

    fixed missing dependency list for modules

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ const {defaults} = require('jest-config');
 module.exports = {
     ...defaults,
     transform: {"\\.js$": "babel-jest"},
-    transformIgnorePatterns: ["/node_modules/(?!(ol|ol-mapbox-style|jsts|geotiff|quick-lru)/)"],
+    transformIgnorePatterns: ["/node_modules/(?!(ol|ol-mapbox-style|jsts|geotiff|quick-lru|rbush|quickselect|pbf)/)"],
     moduleDirectories: ["./src", "./node_modules"],
     setupFiles: ["./tests/setup.js"],
     testEnvironment: "jsdom",
