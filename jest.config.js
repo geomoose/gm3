67fce4cf6fe6166f53b308e9cfa64535e60c3375
Duck Little <theduckylittle@gmail.com>
2026-05-29 15:16:29 -0500

    add usng-map-collar to the modules list

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ const {defaults} = require('jest-config');
 module.exports = {
     ...defaults,
     transform: {"\\.js$": "babel-jest"},
-    transformIgnorePatterns: ["/node_modules/(?!(ol|ol-mapbox-style|jsts|geotiff|quick-lru|rbush|quickselect|pbf)/)"],
+    transformIgnorePatterns: ["/node_modules/(?!(ol|ol-mapbox-style|jsts|geotiff|quick-lru|rbush|quickselect|pbf|usng-map-collar)/)"],
     moduleDirectories: ["./src", "./node_modules"],
     setupFiles: ["./tests/setup.js"],
     testEnvironment: "jsdom",
