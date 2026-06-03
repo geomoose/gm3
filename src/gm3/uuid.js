2cbe5e80599ac755288c50a9bb7bd3287d086feb
Duck Little <theduckylittle@gmail.com>
2026-06-02 19:55:45 -0500

    fix: Drop use of crypto.randomUUID
    This fixes ID generation for HTTP hosted sites (common for internal deployments)

--- src/gm3/uuid.js
+++ src/gm3/uuid.js
@@ -1,8 +1,15 @@
 /**
- * Small shim over uuid generation that uses crypto.randomUUID()
+ * Small shim over uuid generation that uses an internal counter
+ * to generate sequential but unique to the session IDs.
  */
 
+// "GeoMoose" in hex. or an attempt
+let COUNTER = 0x9e03005e;
+
 export const uuid = () => {
-  // This has been broadly supported in the browser since 2021
-  return crypto.randomUUID();
+  // crypto.randomUUID was used before but internal sites are frequently
+  //  deployed with HTTP. This was preventing IDs from being generated.
+  // There are corner cases where this could fail (merging datasets, using _uuid with WFS)
+  //  but the security risk of the UUID library was more problematic.
+  return `${(++COUNTER).toString(16)}-${Math.floor(Math.random() * 10000).toString(16)}`;
 };
